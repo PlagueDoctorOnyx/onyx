668b69da7110695f74d68ebbbed51f71f68a9688
Prosser Lukas <prosser.lukas@t-online.de>
2024-12-08 18:22:13 +0100

    #navigationMain: Navigationsmenü einfügen

---
+++ resources/views/forum/forumOverview.blade.php
@@ -0,0 +1,12 @@
+<x-navigation></x-navigation>
+
+<div class="container">
+    <div>
+        <p>SomeText</p>
+    </div>
+    <div>
+        <p>{{ $path }}</p>
+    </div>
+</div>
+
+<x-footer></x-footer>
