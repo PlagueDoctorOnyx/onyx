d2e53196548e4e60b30a567c01e0dc60e13e945f
Prosser Lukas <prosser.lukas@t-online.de>
2024-12-08 18:42:43 +0100

    kleine änderungen

--- resources/views/forum/forumOverview.blade.php
+++ resources/views/forum/forumOverview.blade.php
@@ -1,11 +1,15 @@
 <x-navigation></x-navigation>
 
-<div class="container">
-    <div>
-        <p>SomeText</p>
+<div class="container mt-3">
+    <h2>Forum</h2>
+</div>
+
+<div class="container mt-5 border">
+    <div class="row bs-light border-bottom">
+        <h3>Info</h3>
     </div>
-    <div>
-        <p>{{ $path }}</p>
+    <div class="row">
+        <p>Wilkommen beim Forum! Hier hast du einen Überblick über alle möglichen Themen</p>
     </div>
 </div>
 
