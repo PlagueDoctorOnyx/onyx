2f77b474ca1c07510552c681a12b2a4d8ab0af8e
Prosser Lukas <prosser.lukas@t-online.de>
2024-12-15 19:06:08 +0100

    Installed breeze

--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-<x-navigation></x-navigation>
-
-<h1>Hi there</h1>
-
-<x-footer></x-footer>
--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,27 @@
 <?php
 
-use App\Http\Controllers\ForumController;
-use App\Models\Thread;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-// Forum
-
-Route::get('/forum', function () {
-    return view('forum/forumOverview');
-});
-
-Route::get('/forum/stories', function () {
-    return view('forum/forumStories', [
-        'data' => Thread::get()
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
 
-Route::controller(ForumController::class)->group(function () {
-    Route::get('/forum/stories/{id}', 'showStory');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-// Profil
-
-Route::get('/profile', function () {
-    return view('');
-});
-
-
+require __DIR__.'/auth.php';
